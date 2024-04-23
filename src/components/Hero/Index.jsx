759290b2393ecca8d4285fec24f8f15fcd693439
Renato Franco <renato_ny@live.com>
2024-04-22 22:45:51 -0300

    feature: initial commit.

---
+++ src/components/Hero/Index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.scss';
+
+const Hero = () => {
+    return(
+        <section className="hero">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="bold title">Fronteiratech</h1>
+                        <p className="subtitle">A primeira comunidade tech de São Borja.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default React.memo(Hero);
